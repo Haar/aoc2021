26ad078e373dbc4f13733671cb0f510fc755b872
Robert White <robert.white@skybettingandgaming.com>
2021-12-05 13:05:24 +0000

    Day 2: Dive! (Elixir)
    - Star 1 - `Enum.reduce` to calculate increasing values
    - Star 2 - Same as Star 1 but tweak the calculation

---
+++ day-2/star-1/main.exs
@@ -0,0 +1,38 @@
+defmodule Submarine do
+  def calculate_destination(instructions) do
+    instructions
+    |> Enum.reduce({0, 0}, &apply_instruction/2)
+    |> Tuple.product()
+  end
+
+  def apply_instruction(instruction, {x, y}) do
+    [direction, magnitude] = String.split(instruction, " ", trim: true)
+
+    magnitude = String.to_integer(magnitude)
+
+    case direction do
+      "up"      -> {x, y - magnitude}
+      "down"    -> {x, y + magnitude}
+      "forward" -> {x + magnitude, y}
+    end
+  end
+end
+
+# Test
+# """
+# forward 5
+# down 5
+# forward 8
+# up 3
+# down 8
+# forward 2
+# """
+# |> String.split("\n", trim: true)
+# |> Submarine.calculate_destination()
+# |> IO.puts()
+
+"./input.txt"
+|> File.stream!()
+|> Enum.map(&String.trim/1)
+|> Submarine.calculate_destination()
+|> IO.puts()
